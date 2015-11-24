a2c9f2b9a80884bbfc2fb71d25a6fe9e9eed6339
lml <liuminglu19870419@163.com>
2015-11-24 20:54:37 +0800

    modify the mysql database change the type of create)time from integer to bigint

--- crawler/sql&script/create_table.sql
+++ crawler/sql&script/create_table.sql
@@ -1,37 +1,37 @@
-DROP TABLE IF EXISTS `test`.`published_url`;
-CREATE TABLE  `test`.`published_url` (
-  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
-  `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `version` varchar(45) NOT NULL,
-  `count` int(10) unsigned NOT NULL DEFAULT '1',
-  UNIQUE KEY `Index_url` (`url`) USING HASH,
-  KEY `Index_time` (`create_time`) USING BTREE,
-  KEY `Index_id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=ascii;
 DROP TABLE IF EXISTS `test`.`successed_url`;
-CREATE TABLE  `test`.`published_url` (
-  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
+CREATE TABLE  `test`.`successed_url` (
+  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0',
   `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `version` varchar(45) NOT NULL,
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `version` varchar(45) NOT NULL DEFAULT '',
   `count` int(10) unsigned NOT NULL DEFAULT '1',
   UNIQUE KEY `Index_url` (`url`) USING HASH,
   KEY `Index_time` (`create_time`) USING BTREE,
   KEY `Index_id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ascii;
 DROP TABLE IF EXISTS `test`.`failed_url`;
-CREATE TABLE  `test`.`published_url` (
-  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
+CREATE TABLE  `test`.`failed_url` (
+  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0',
   `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `version` varchar(45) NOT NULL,
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `version` varchar(45) NOT NULL DEFAULT '',
+  `count` int(10) unsigned NOT NULL DEFAULT '1',
+  UNIQUE KEY `Index_url` (`url`) USING HASH,
+  KEY `Index_time` (`create_time`) USING BTREE,
+  KEY `Index_id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=ascii;
+DROP TABLE IF EXISTS `test`.`published_url`;
+CREATE TABLE  `test`.`published_url` (
+  `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `version` varchar(45) NOT NULL DEFAULT '',
   `count` int(10) unsigned NOT NULL DEFAULT '1',
   UNIQUE KEY `Index_url` (`url`) USING HASH,
   KEY `Index_time` (`create_time`) USING BTREE,
--- crawler/sql&script/create_table.sql~
+++ crawler/sql&script/create_table.sql~
@@ -1,24 +1,39 @@
-CREATE TABLE `published_url` (
+DROP TABLE IF EXISTS `test`.`published_url`;
+CREATE TABLE  `test`.`published_url` (
   `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
   `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL DEFAULT '0',
-  UNIQUE KEY `Index_url` (`url`) USING HASH
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `version` varchar(45) NOT NULL,
+  `count` int(10) unsigned NOT NULL DEFAULT '1',
+  UNIQUE KEY `Index_url` (`url`) USING HASH,
+  KEY `Index_time` (`create_time`) USING BTREE,
+  KEY `Index_id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ascii;
-CREATE TABLE `successed_url` (
+DROP TABLE IF EXISTS `test`.`successed_url`;
+CREATE TABLE  `test`.`published_url` (
   `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
   `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL DEFAULT '0',
-  UNIQUE KEY `Index_url` (`url`) USING HASH
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `version` varchar(45) NOT NULL,
+  `count` int(10) unsigned NOT NULL DEFAULT '1',
+  UNIQUE KEY `Index_url` (`url`) USING HASH,
+  KEY `Index_time` (`create_time`) USING BTREE,
+  KEY `Index_id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ascii;
-CREATE TABLE `failed_url` (
+DROP TABLE IF EXISTS `test`.`failed_url`;
+CREATE TABLE  `test`.`published_url` (
   `url` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
   `tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `sub_tag` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `id` int(10) unsigned NOT NULL DEFAULT '0',
-  UNIQUE KEY `Index_url` (`url`) USING HASH
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `version` varchar(45) NOT NULL,
+  `count` int(10) unsigned NOT NULL DEFAULT '1',
+  UNIQUE KEY `Index_url` (`url`) USING HASH,
+  KEY `Index_time` (`create_time`) USING BTREE,
+  KEY `Index_id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=ascii;
