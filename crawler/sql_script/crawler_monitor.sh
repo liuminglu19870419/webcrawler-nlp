3dca60f2bb86186ca69ee1f6c1a73dc26704fb17
lml <liuminglu19870419@163.com>
2015-11-30 22:21:45 +0800

    fix the crawler monitor if statment bug

--- crawler/sql_script/crawler_monitor.sh
+++ crawler/sql_script/crawler_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 procID=`ps -aux | awk '$11=="python"{print $1,$2, $11,$12}' | grep 'crawler_run'`
-if [ "" != "$procID" ];
+if [ "" == "$procID" ];
 then
 killall -9 phantomjs
 python /home/lml/webcrawler/webcrawler-nlp/crawler/crawler/crawler_run.py start
