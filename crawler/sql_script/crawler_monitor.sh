6c8f3f8d81f51b64d5c35d3ca98106a2c3063d4a
lml <liuminglu19870419@163.com>
2015-12-06 10:06:21 +0800

    modify crawler_monitor logic

--- crawler/sql_script/crawler_monitor.sh
+++ crawler/sql_script/crawler_monitor.sh
@@ -3,6 +3,7 @@ procID=`ps -aux | awk '$11=="python"{print $1,$2, $11,$12}' | grep 'crawler_run'
 if [ "" == "$procID" ];
 then
 killall -9 phantomjs
+killall -9 python
 python /home/lml/webcrawler/webcrawler-nlp/crawler/crawler/crawler_run.py start
 fi
 
