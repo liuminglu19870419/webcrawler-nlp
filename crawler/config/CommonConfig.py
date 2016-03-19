dcc3c3adcaefe55db45da7f8410084ca369f309a
lml <liuminglu19870419@163.com>
2016-03-19 08:47:36 +0800

    new virtual machine test

--- crawler/config/CommonConfig.py
+++ crawler/config/CommonConfig.py
@@ -9,9 +9,9 @@ LOG_PATH = "/home/lml/log/"
 
 PHANTOMJS_PATH = "/home/lml/phantomjs/bin/phantomjs"
 NEWS_URL_QUEUE = "news_article"
-# HOST_IP = "192.168.1.100"
-HOST_IP = "127.0.0.1"
-CRAWLER_THREAD_COUNT = 4
+HOST_IP = "192.168.1.100"
+# HOST_IP = "127.0.0.1"
+CRAWLER_THREAD_COUNT = 2
 MONGO_IP = HOST_IP
 RABBITMQ_IP = HOST_IP
 MYSQL_IP = HOST_IP
